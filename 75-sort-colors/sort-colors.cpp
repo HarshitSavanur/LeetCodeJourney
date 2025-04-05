d8aa94edc2352e31b25cb6b6ba6a8fe5785c93d1
Harshit Savanur <116423483+HarshitSavanur@users.noreply.github.com>
2025-04-05 17:06:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (75.55%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,34 +1,51 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int l=0;
-        int r=nums.size()-1;
-        mergeSort(nums,l,r);
-    }
-    void mergeSort(vector<int>& arr, int l, int r) {
-        // code here
-        if(l>=r) return;
-        int m=l+(r-l) / 2;
-        
-        mergeSort(arr,l,m);
-        mergeSort(arr,m+1,r);
-        merge(arr,l,m,r);
-    }
-    void merge(vector<int> &arr, int l, int m, int r){
-        vector <int> a1,a2;
-        for(int i=l;i<=m;i++) a1.push_back(arr[i]);
-        for(int j=m+1;j<=r;j++) a2.push_back(arr[j]);
-        
-        int i=0,j=0,k=l;
-        while(i<a1.size() && j<a2.size()){
-            if(a1[i]<=a2[j]) arr[k++]=a1[i++];
-            else arr[k++]=a2[j++];
-        }
-        while(i<a1.size()){
-            arr[k++]=a1[i++];
-        }
-        while(j<a2.size()){
-            arr[k++]=a2[j++];
+        int n = nums.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
         }
     }
 };
+//brute force appproach
+// int l=0;
+//         int r=nums.size()-1;
+//         mergeSort(nums,l,r);
+//     }
+//     void mergeSort(vector<int>& arr, int l, int r) {
+//         // code here
+//         if(l>=r) return;
+//         int m=l+(r-l) / 2;
+        
+//         mergeSort(arr,l,m);
+//         mergeSort(arr,m+1,r);
+//         merge(arr,l,m,r);
+//     }
+//     void merge(vector<int> &arr, int l, int m, int r){
+//         vector <int> a1,a2;
+//         for(int i=l;i<=m;i++) a1.push_back(arr[i]);
+//         for(int j=m+1;j<=r;j++) a2.push_back(arr[j]);
+        
+//         int i=0,j=0,k=l;
+//         while(i<a1.size() && j<a2.size()){
+//             if(a1[i]<=a2[j]) arr[k++]=a1[i++];
+//             else arr[k++]=a2[j++];
+//         }
+//         while(i<a1.size()){
+//             arr[k++]=a1[i++];
+//         }
+//         while(j<a2.size()){
+//             arr[k++]=a2[j++];
+//         }
